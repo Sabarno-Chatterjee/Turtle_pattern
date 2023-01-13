b19ddcacc81a4c26fb36aa523a96abd2cce6ad3e
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-13 20:48:35 +0530

    Edit main.py; used setheading() for randomwalk

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def shape_shifter(num_of_sides):
 
 tim = Turtle()
 tim.shape("turtle")
-tim.pensize(5)
+tim.pensize(10)
 tim.speed(0)
 colours = ["red", "black", "blue", "green", "yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple",
            "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown",
@@ -19,17 +19,21 @@ colours = ["red", "black", "blue", "green", "yellow", "gold", "orange", "red", "
 # for num_of_sides in range(3, 11):
 #     tim.pencolor(random.choice(colours))
 #     shape_shifter(num_of_sides)
-for movement in range(100000):
-    tim.color(random.choice(colours))
-    direction = random.randint(0,1)
-    if direction:
-        tim.left(90)
-        tim.forward(20)
-    else:
-        tim.right(90)
-        tim.forward(20)
+# for movement in range(100000):
+#     tim.color(random.choice(colours))
+#     direction = random.randint(0,1)
+#     if direction:
+#         tim.left(90)
+#         tim.forward(20)
+#     else:
+#         tim.right(90)
+#         tim.forward(20)
 
-
+directions = [0, 90, 180, 270]
+for _ in range(300):
+    tim.pencolor(random.choice(colours))
+    tim.setheading(random.choice(directions))
+    tim.forward(30)
 
 screen = Screen()
 screen.exitonclick()
