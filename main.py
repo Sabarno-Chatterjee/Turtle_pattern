2703ca2840c3a81958e68a8b16349c5730a471ca
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-15 20:35:22 +0530

    Edit main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import turtle
-
 import colorgram
 from turtle import Turtle as t
 import random
-
 
 turtle.colormode(255)
 # rgb_colors = []
@@ -15,9 +13,11 @@ turtle.colormode(255)
 #     b = color.rgb.b
 #     new_shade = (r, g, b)
 #     rgb_colors.append(new_shade)
-design_shades = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
-
-# def colour_generator():
+design_shades = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20),
+                 (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70),
+                 (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74),
+                 (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153),
+                 (176, 192, 208), (168, 99, 102)]
 
 tim = t()
 my_screen = tim.getscreen()
@@ -25,32 +25,20 @@ tim.hideturtle()
 tim.penup()
 tim.speed(0)
 
-# def design_pattern():
-#     for _ in range(100):
-#         color = random.choice(design_shades)
-#         tim.color(color)
-#         tim.dot(20)
-#         tim.forward(50)
-
-
-# design_pattern()
 tim.setheading(225)
 tim.forward(300)
 tim.setheading(0)
+number_of_dots = 100
 
-for _ in range(10):
+for count_dots in range(1, number_of_dots + 1):
     color = random.choice(design_shades)
-    tim.dot(10,color)
+    tim.dot(10, color)
     tim.forward(50)
-for _ in range(9):
-    tim.setheading(90)
-    tim.forward(50)
-    tim.setheading(180)
-    tim.forward(500)
-    tim.setheading(0)
-    for _ in range(10):
-        color = random.choice(design_shades)
-        tim.dot(10,color)
+    if count_dots % 10 == 0:
+        tim.setheading(90)
         tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 turtle.exitonclick()
