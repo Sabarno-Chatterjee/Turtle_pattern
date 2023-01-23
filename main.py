ebbef3d0305815dbfbed528b5c0f91fe8b64846c
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-23 11:51:34 +0530

    edit main.py

--- main.py
+++ main.py
@@ -4,15 +4,7 @@ from turtle import Turtle as t
 import random
 
 turtle.colormode(255)
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     # rgb_colors.append(color.rgb)
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_shade = (r, g, b)
-#     rgb_colors.append(new_shade)
+
 design_shades = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20),
                  (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70),
                  (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74),
